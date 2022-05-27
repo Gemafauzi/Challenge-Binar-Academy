329b7ee40a62af638be21bc71e9f7f7c53ea53ce
Gemasss <58686523+Gemafauzi@users.noreply.github.com>
2022-05-27 20:02:46 +0700

    footer-navbar-chart-input

--- CHALLENGE-07/components/Navbar/Navbar.js
+++ CHALLENGE-07/components/Navbar/Navbar.js
@@ -5,17 +5,17 @@ import styles from './navbar.module.css'
 export default function Navbar() {
   return (
     <div>
-        <div className={styles.navbar}>
-            <div className={styles.logo}>FEJS2</div>
-            <div>
-                <ul className={styles.list}>
-                    <li className={styles.item}><Link href="/" style={{textDecoration: 'none'}}>Home</Link></li>
-                    <li className={styles.item}><Link href="/input">Input</Link></li>
-                    <li className={styles.item}><Link href="/chart">Chart</Link></li>
-                    <li className={styles.item}><Link href="/download">Download</Link></li>
-                </ul>
-            </div>
+      <div className={styles.navbar}>
+        <div className={styles.logo}>FEJS2</div>
+        <div>
+          <ul className={styles.list}>
+            <li className={styles.item}><Link href="/" style={{ textDecoration: 'none' }}>Home</Link></li>
+            <li className={styles.item}><Link href="/input">Input</Link></li>
+            <li className={styles.item}><Link href="/chart">Chart</Link></li>
+            <li className={styles.item}><Link href="/download">Download</Link></li>
+          </ul>
         </div>
+      </div>
     </div>
   )
 }
